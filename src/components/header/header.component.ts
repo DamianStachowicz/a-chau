1d6ccb59960d59a6cb7acb759e6cf01eae0cc8e4
Damian Stachowicz-Le <d.m.stachowicz@gmail.com>
2025-08-27 11:54:57 +0200

    Update header component to use ui-button for navigation links and adjust header height variable

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ButtonComponent } from '../../ui/button/button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
