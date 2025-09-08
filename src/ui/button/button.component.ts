659adfe102b0f81aaad311496a655e1864a88519
Damian Stachowicz-Le <d.m.stachowicz@gmail.com>
2025-09-08 15:07:41 +0200

    Add routerLink support to ui-button and update home component usage

--- src/ui/button/button.component.ts
+++ src/ui/button/button.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ui-button',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
   public disabled: InputSignal<boolean> = input<boolean>(false);
+  public routerLink: InputSignal<string | any[]> = input<string | any[]>('');
 }
