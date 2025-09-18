777445e7940886c4ba88dd3f339aa0269297cd0c
Damian Stachowicz-Le <d.m.stachowicz@gmail.com>
2025-09-18 12:17:17 +0200

    Add tests for footer component

--- src/components/footer/footer.component.spec.ts
+++ src/components/footer/footer.component.spec.ts
@@ -1,22 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FooterComponent } from './footer.component';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-import { Footer } from './footer';
-
-describe('Footer', () => {
-  let component: Footer;
-  let fixture: ComponentFixture<Footer>;
+describe('FooterComponent', () => {
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Footer],
+      imports: [FooterComponent],
+      providers: [provideZonelessChangeDetection()],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Footer);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(FooterComponent);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have footer__contact element', () => {
+    const contactElement = fixture.debugElement.query(By.css('.footer__contact'));
+    expect(contactElement).toBeTruthy();
+  });
+
+  it('should have footer__opening-hours element', () => {
+    const openingHoursElement = fixture.debugElement.query(By.css('.footer__opening-hours'));
+    expect(openingHoursElement).toBeTruthy();
+  });
+
+  it('should have footer__socials element', () => {
+    const socialsElement = fixture.debugElement.query(By.css('.footer__socials'));
+    expect(socialsElement).toBeTruthy();
   });
 });
