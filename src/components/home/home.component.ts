b14c73d41e0dffd9e24a2851e939e979c5aa6a3a
Damian Stachowicz-Le <d.m.stachowicz@gmail.com>
2025-10-04 15:39:17 +0200

    Expand homepage

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,12 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonComponent } from '../../ui/button/button.component';
+import { MapComponent } from '../map/map.component';
+import { MapLocation } from '../map/map-location.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, MapComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public location: MapLocation = {
+    latitude: 52.22870140039739,
+    longitude: 20.90223476567207,
+    name: 'Restauracja A Chau',
+  };
+}
