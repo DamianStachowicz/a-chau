9113bb02d8e4bcdb6dae3baa752ab91439838501
Damian Stachowicz-Le <d.m.stachowicz@gmail.com>
2026-08-30 12:15:47 +0200

    feat: add footer component with styles for mobile

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
+  imports: [FooterComponent],
 })
 export class App {}
