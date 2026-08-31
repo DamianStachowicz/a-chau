72ae113de57e46114a1166ced588f36e44238cb0
Damian Stachowicz-Le <d.m.stachowicz@gmail.com>
2026-09-01 00:10:11 +0200

    feat: implement Home page with hero section only

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  imports: [FooterComponent],
+  imports: [FooterComponent, RouterModule],
 })
 export class App {}
